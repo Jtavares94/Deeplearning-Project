cba871f76071cdb4378d62d16b9b539037001883
Jeilliano <0919078@hr.nl>
2019-05-20 13:04:52 +0200

    startup project

---
+++ Images.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import glob
+
+image_list = []
+
+images = glob.glob("images/*.jpg")
+for image in images:
+  #im = Image.open(image)
+  #image_list.append(im)
+  with open(image, 'rb') as file:
+    img = Image.open(file)
+    img.show()
+    image_list.append(img)
+
+test = 0
+for img in image_list:
+  test += 1
+  print(test)
